4c5f8768b2c6abb10708f8e4a670b0a8ae5c512f
Suresh Gopalakrishnan <bkgsur@yahoo.com>
2025-05-18 12:05:30 -0700

    Checkin 1

--- src/ukf.h
+++ src/ukf.h
@@ -5,7 +5,7 @@
 #include "measurement_package.h"
 
 class UKF {
- public:
+public:
   /**
    * Constructor
    */
@@ -95,6 +95,19 @@ class UKF {
 
   // Sigma point spreading parameter
   double lambda_;
+
+  // Radar measurement noise covariance matrix
+  Eigen::MatrixXd R_radar_;
+
+  // Lidar measurement noise covariance matrix
+  Eigen::MatrixXd R_lidar_;
+
+  ///* the current NIS for radar
+  double NIS_radar_;
+
+  ///* the current NIS for laser
+  double NIS_laser_;
+
 };
 
 #endif  // UKF_H
